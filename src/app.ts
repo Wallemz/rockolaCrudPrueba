d8677059d6c9d394a93ab8445cf945b835209891
Wallemz <william.pscn@gmail.com>
2021-11-15 12:41:03 -0500

    Configuración inicial 2

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ const port = 3000;                  // Guardamos el puerto, en este caso 300
 
 // RUTAS
 app.get('/', (req, res)=>{          // Función para definir GET (ruta, función(request,response))
-    res.send('GET_RTA: Prueba del servidor'); // Retorno deL GET
+    res.send('GET_RTA: Prueba del servidor.'); // Retorno deL GET
 })
 
 app.listen(port, ()=>{              // Listen es uno de los métodos de express (Puerto, función)
-    return console.log(`INFO: Servidor corriendo en el puerto ${port}`);
+    return console.log(`INFO: Servidor corriendo en el puerto ${port}.`);
 });
