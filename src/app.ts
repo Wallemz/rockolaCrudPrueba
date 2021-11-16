66ca8a27f3fd381f0bec9f2a0c0b2e449d976295
Wallemz <william.pscn@gmail.com>
2021-11-16 15:37:48 -0500

    Fin API canciones

--- src/app.ts
+++ src/app.ts
@@ -15,10 +15,10 @@ app.get('/prueba', async (req, res, next)=>{  // Función para definir GET (ruta
     
     // REQUEST
     // Obtener datos del objeto Req
-//    console.log(req.headers);       // Obtengo los headers de la petición
-//    console.log(req.params);        // Obtengo los params de la petición
-//    console.log(req.query);         // Obtengo los query params de la petición
-//    console.log(req.body);          // Obtengo el body de la petición pero llega undefined, hay que definir que el body llega en formato JSON Ver lineas 7 y 8
+    //    console.log(req.headers);       // Obtengo los headers de la petición
+    //    console.log(req.params);        // Obtengo los params de la petición
+    //    console.log(req.query);         // Obtengo los query params de la petición
+    //    console.log(req.body);          // Obtengo el body de la petición pero llega undefined, hay que definir que el body llega en formato JSON Ver lineas 7 y 8
 
     // RESPONSE
     //res.send('GET_RTA: Prueba del servidor.');  // Retorno de la petición con texto plano
@@ -29,26 +29,45 @@ app.get('/prueba', async (req, res, next)=>{  // Función para definir GET (ruta
     // NEXT
     //next(); // Manda a ejecutar la siguiente ruta
 
-    // PROMESA
-    console.log('Antes de la promesa');
-    let x = 10;
-    const promesa = new Promise((resolve, reject)=>{    // Creamos la promesa,recibe resolve y reject
-        if (x==10){
-            resolve('Promesa resuelta');
-        } else {
-            reject('Priomesa rechazada');
-        }
-    });                             // Valida las posibilidades
+    //// PROMESA
+    //console.log('Antes de la promesa');
+    //let x = 10;
+    //const promesa = new Promise((resolve, reject)=>{    // Creamos la promesa,recibe resolve y reject
+    //    if (x==10){
+    //        resolve('Promesa resuelta');
+    //    } else {
+    //        reject('Priomesa rechazada');
+    //    }
+    //});                             // Valida las posibilidades
+//
+    //// Asincrono
+    //await promesa.then((res)=>{    // Si se cumplió // el await hace  que espere que esa promesa se reseulva
+    //    console.log(res);
+    //}).catch((error)=>{            // Si no se cumplió
+    //    console.log(error);
+    //})
+//
+    //console.log('Después de la promesa');
 
-    // Asincrono
-    await promesa.then((res)=>{    // Si se cumplió // el await hace  que espere que esa promesa se reseulva
-        console.log(res);
-    }).catch((error)=>{            // Si no se cumplió
-        console.log(error);
-    })
+    // DESESTRUCTURACIÓN
+    const datos ={
+        nombre:'tatiana',
+        apellido: 'albarracin',
+        genero:'femenino'
+    }
+    const {apellido} = datos;   // objeto {} array []
+    // Spread Operator
+    const nuevosDatos = {
+        //nombre: datos.nombre,
+        //apellido: datos.apellido,
+        //genero: datos.genero,
+        ...datos,               // En vez de escribir todo lo de arriba, con los ... me trae todo de datos, reemplaza los datos en caso de que se repita una clave
+        ciudad: 'Bucaramanga',
+        profesion:'Ing Sistemas'
+    }
+    
 
-    console.log('Después de la promesa');
-    res.status(200).json({nombre:"William"});
+    res.status(200).json({datos:apellido,nuevosDatos});
 })
 
 // LISTEN DEL SERVICIO
--- src/controllers/cancionesController.ts
+++ src/controllers/cancionesController.ts
@@ -8,23 +8,65 @@ const obtenerCanciones = (req, res) =>{
         }
         res.json(data);     // Si logra la petición devuelve el response
     }).catch((error)=>{
+        console.log(error);
         res.status(500).send(error);        // Si no se logra la petición error
     });
 }
 
 const obtenerCancion = (req, res) =>{
-    res.send('obtenerCancion desde el controlador');
+    const {id} = req.params;    // Desestructuración
+    executeQuery(`SELECT * FROM canciones WHERE idcanciones = ${id}`).then((response)=>{ // Como es una promesa debe terminar en .then y catch
+        const data ={
+            message: `${response.length} datos encontrados.`,
+            datos: response.length > 0 ? response[0] : null   // IF Ternario
+        }
+        res.json(data);     // Si logra la petición devuelve el response
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).send(error);        // Si no se logra la petición error
+    });
 }
 
-const agregarCancion = (req, res) =>{
-    res.send('Obtener canciones desde el controlador');
+const agregarCancion = async(req, res) =>{
+    const {nombre, genero, artista} = req.body;    // Desestructuración
+    try {
+        const response = await executeQuery(`INSERT INTO canciones (nombre, genero, artista) VALUES ('${nombre}','${genero}','${artista}')`);
+        console.log(response)
+        res.status(201).json({message:'created', id: response.insertId});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);        // Si no se logra la petición error
+    }
 }
 
-const actualizarCancion = (req, res) =>{
-    res.send('agregarCancion desde el controlador');
+const actualizarCancion = async(req, res) =>{
+    const {nombre, genero, artista} = req.body;    // Desestructuración
+    try {
+        const response = await executeQuery(`UPDATE canciones SET nombre = '${nombre}', genero = '${genero}', artista = '${artista}' WHERE idcanciones = ${req.params.id}`);
+        console.log(response)
+        if(response.affectedRows > 0){
+            res.json({message:'updated'});
+        }else{
+            res.status(404).json({message:`No existe registro con id: ${req.params.id}`});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);        // Si no se logra la petición error
+    }
 }
 
-const eliminarCancion = (req, res) =>{
-    res.send('eliminarCancion desde el controlador');
+const eliminarCancion = async(req, res) =>{
+    try {
+        const response = await executeQuery(`DELETE FROM canciones WHERE idcanciones = ${req.params.id}`);
+        console.log(response)
+        if(response.affectedRows > 0){
+            res.json({message:'deleted'});
+        }else{
+            res.status(404).json({message:`No existe registro con id: ${req.params.id}`});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);        // Si no se logra la petición error
+    }
 }
 export {obtenerCanciones, obtenerCancion, agregarCancion, actualizarCancion, eliminarCancion}
